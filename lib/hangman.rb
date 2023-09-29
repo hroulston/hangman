9d511f661a78f747fdc1fc4992333b90f8fc8691
hroulston <h_roulston@live.com>
2023-09-29 10:46:49 -0500

    Works on figuring out logic to get letter guess from user, then compare against key word and display if it is in the word or not.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,6 @@
 
+# Takes file and sorts out words that are 5-12 characters long
+# Chooses a random word from the array of selected words
 def secret_word
     contents = File.open('google-10000-english-no-swears.txt', 'r')
     content_array = contents.readlines.map(&:chomp)
@@ -12,9 +14,27 @@ def secret_word
     selected_words.sample
 end 
 
-# display
-key_word = secret_word()
-blanked_key_word = Array.new(key_word.length, "_")
-puts blanked_key_word.join(" ")
+# Takes random word and blanks out letters
+# Displays _ _ _ _ to the user
+def display_secret_word
+    blanked_key_word = Array.new(secret_word().length, "_")
+    puts blanked_key_word.join(" ")
+end
 
+# Figuring out logic to get letter from user and check if 
+# that letter is in the secret word, then display letter
+# position to the user.
+key_word = secret_word()
+blanked_word = display_secret_word()
+
+puts "Guess a letter"
+answer = gets.chomp
+
+array_key_word = key_word.split("")
+array_blanked_key_word = blanked_key_word.split("")
+
+array_key_word.each do |letter|
+    if answer == letter
+
+end
 puts "Hangman Game"
