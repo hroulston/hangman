cea75fa6f2b72d7e3dc24516257667a5c6d7ed5c
hroulston <h_roulston@live.com>
2023-09-29 08:19:57 -0500

    Parses through file and pulls out words that are 5-12 characters in length.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,16 +1,16 @@
-#Consider doing a for each method on the file and pulling the method
-#out and calling it within for each method.
-contents = File.open('google-10000-english-no-swears.txt')
+
 def word_count_six_to_eleven(words)
-    selected_words = []
-    array_words = words.split("/n")
-    array_words.each do |word|
-        if word.length >= 5 && word.length <= 12
-            selected_words.push(word)
-        end
-    end
-    selected_words
 end   
 
+
+contents = File.open('google-10000-english-no-swears.txt', 'r')
+content_array = contents.readlines.map(&:chomp)
+selected_words = []
+content_array.each do |word|
+    if word.length >= 5 && word.length <= 12
+        selected_words.push(word)
+    end
+contents.close
+puts selected_words
+end
 puts "Hangman Game"
-puts "#{word_count_six_to_eleven(contents)}"
