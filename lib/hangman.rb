d1819d7f598c6f22dd7583285293f82f2e6c0958
hroulston <h_roulston@live.com>
2023-09-29 08:41:27 -0500

    Takes secret word and dispalys lines to user instead of letters.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,16 +1,20 @@
 
-def word_count_six_to_eleven(words)
-end   
+def secret_word
+    contents = File.open('google-10000-english-no-swears.txt', 'r')
+    content_array = contents.readlines.map(&:chomp)
+    selected_words = []
+        content_array.each do |word|
+            if word.length >= 5 && word.length <= 12
+                selected_words.push(word)
+            end
+        contents.close
+        end
+    selected_words.sample
+end 
 
+# display
+key_word = secret_word()
+blanked_key_word = Array.new(key_word.length, "_")
+puts blanked_key_word.join(" ")
 
-contents = File.open('google-10000-english-no-swears.txt', 'r')
-content_array = contents.readlines.map(&:chomp)
-selected_words = []
-content_array.each do |word|
-    if word.length >= 5 && word.length <= 12
-        selected_words.push(word)
-    end
-contents.close
-puts selected_words
-end
 puts "Hangman Game"
